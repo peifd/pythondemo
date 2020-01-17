4663ea2fac58711f1f6b4eac496b7a573c1ef947
peifd <peifandong@163.com>
2020-01-17 18:26:11 +0800

    rjuq

---
+++ zybank/abstest.py
@@ -0,0 +1,11 @@
+def my_abs(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError("bad operand type")
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+
+if __name__ == "__main__":
+    my_abs(-4)
--- zybank/test.py
+++ zybank/test.py
@@ -1,2 +1,22 @@
-print "hello"
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+'''
+python 学习demo
+author:peifd
+E-mail:peifandong@163.com
+Tel:13643839903
+'''
+from io import StringIO, BytesIO
+
+import time, threading, multiprocessing
+from multiprocessing import Process
+from datetime import datetime, timedelta
+import time
+from collections import *
+import base64
+import hashlib
+import hmac
+import psutil
+
+print psutil.pids()
